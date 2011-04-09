5afa9c6388463f05dc13f293316350318df8b994
Cloudef <mailRoxas@gmail.com>
2011-04-09 18:38:57 +0300

    Twinview updates

--- .config/conky/start_conky.sh
+++ .config/conky/start_conky.sh
@@ -3,4 +3,3 @@
 sleep 2
 conky -q -d -c $HOME/.config/conky/monitor.conky &
 (conky -q -d -c $HOME/.config/conky/calendar.conky && conky -q -d -c $HOME/.config/conky/db.conky) &
-conky -q -d -c $HOME/.config/conky/time.conky &
