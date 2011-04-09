bf07d4753025e27b6d9630c842f4e814c8daba41
Cloudef <mailRoxas@gmail.com>
2011-04-09 19:37:57 +0300

    Conky changes

--- .config/conky/start_conky.sh
+++ .config/conky/start_conky.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 sleep 2
-conky -q -d -c $HOME/.config/conky/monitor.conky &
+conky -q -d -c $HOME/.config/conky/clock.conky &
 (conky -q -d -c $HOME/.config/conky/calendar.conky && conky -q -d -c $HOME/.config/conky/db.conky) &
