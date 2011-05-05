79ec71bfd699f3b95ceb460d2b99039f9010614a
Cloudef <mailRoxas@gmail.com>
2011-05-05 17:58:41 +0300

    Dark look again

--- .config/conky/start_conky.sh
+++ .config/conky/start_conky.sh
@@ -2,4 +2,4 @@
 
 sleep 2
 conky -q -d -c $HOME/.config/conky/clock.conky &
-(conky -q -d -c $HOME/.config/conky/calendar.conky && conky -q -d -c $HOME/.config/conky/db.conky) &
+conky -q -d -c $HOME/.config/conky/db.conky &
