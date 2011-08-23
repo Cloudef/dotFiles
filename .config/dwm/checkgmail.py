c74872c415345273d229562ed7180c1833ea04fe
Cloudef <mailRoxas@gmail.com>
2011-08-23 22:13:29 +0300

    switch to xcompmgr

--- .config/dwm/checkgmail.py
+++ .config/dwm/checkgmail.py
@@ -42,8 +42,8 @@ def getOptions(parser):
                       help="Your gmail password")
     parser.add_option("-m", "--messages", dest="messages", default=5,
                       help="Number of messages to show, default is 5")
-    parser.add_option("-l", "--length", dest="headerlength",default=3,
-                      help="Number of words in mail header, default is 3")
+    parser.add_option("-l", "--length", dest="headerlength",default=42,
+                      help="Number of character in mail header, default is 42")
     options, args = parser.parse_args()
 
     # lets do some checking
@@ -94,13 +94,15 @@ def showmail(feed):
     #then print the messages
     for i in range(min(messages,newmails)):
 
-        emailtitle = atom.entries[i].title
+        emailtitle  = atom.entries[i].title.encode("utf-8")
+        emailauthor = atom.entries[i].author.encode("utf-8")
+        length = headerlength - len(emailauthor)
         # show only first few words if title is too long
-        if len(emailtitle.split()) > headerlength:
-            emailtitle = ' '.join(emailtitle.split()[:headerlength])
+        if len(emailtitle) > length:
+         emailtitle = emailtitle[:length]
 
-        print "^fg(%s)%40s [%s]" % (plaincolor, emailtitle, atom.entries[i].author)
-    else:
+        print "^fg(%s)%s [%s]" % (plaincolor, emailtitle,emailauthor)
+    if newmails == 0:
         print ""
         print ""
         print "Nothing here"
